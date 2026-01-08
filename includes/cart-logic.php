d73d12cd5fc5598bbb17aa05540179a411bb5668
eliranmazoz65-debug <eliranmazoz65@gmail.com>
2026-01-08 20:07:08 +0200

    Update cart-logic.php

--- includes/cart-logic.php
+++ includes/cart-logic.php
@@ -1,23 +1,29 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-// שמירת נתונים לסל
+// 1. שמירת המידע לסל הקניות
 add_filter( 'woocommerce_add_cart_item_data', function( $cart_item_data, $product_id ) {
-    if ( ! empty( $_POST['jmp_use_back'] ) ) {
-        $cart_item_data['jmp_fee'] = $_POST['jmp_back_fee'];
-        $cart_item_data['jmp_back_text'] = $_POST['jmp_back_text'];
-    }
-    if ( ! empty( $_POST['jmp_front_text'] ) ) {
-        $cart_item_data['jmp_front_text'] = $_POST['jmp_front_text'];
+    if ( isset( $_POST['jmp_val'] ) ) {
+        $cart_item_data['custom_jewelry_options'] = $_POST['jmp_val'];
     }
     return $cart_item_data;
 }, 10, 2 );
 
-// עדכון המחיר בעגלה
-add_action( 'woocommerce_before_calculate_totals', function( $cart ) {
-    foreach ( $cart->get_cart() as $item ) {
-        if ( isset( $item['jmp_fee'] ) ) {
-            $item['data']->set_price( $item['data']->get_price() + floatval($item['jmp_fee']) );
+// 2. הצגת המידע בתוך עמוד ההזמנה של מנהל האתר
+add_action( 'woocommerce_checkout_create_order_line_item', function( $item, $cart_item_key, $values, $order ) {
+    if ( isset( $values['custom_jewelry_options'] ) ) {
+        foreach ( $values['custom_jewelry_options'] as $label => $value ) {
+            if(!empty($value) && $value !== 'לא') {
+                $item->add_meta_data( $label, $value );
+            }
         }
+    }
+}, 10, 4 );
+
+// 3. עדכון המחיר הסופי בעגלה לפי בחירות הלקוח
+add_action( 'woocommerce_before_calculate_totals', function( $cart ) {
+    if ( is_admin() && ! defined( 'DOING_AJAX' ) ) return;
+    foreach ( $cart->get_cart() as $item ) {
+        // כאן ניתן להוסיף לוגיקה לחישוב מחיר נוסף אם צריך
     }
 });
