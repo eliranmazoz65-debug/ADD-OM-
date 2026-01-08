5e7c43e83f372de470fb820fc0bce4d66a2ae4fc
eliranmazoz65-debug <eliranmazoz65@gmail.com>
2026-01-08 12:25:42 +0200

    Update jewelry-master-pro.php

--- jewelry-master-pro.php
+++ jewelry-master-pro.php
@@ -1,1 +1,13 @@
+<?php
+/**
+ * Plugin Name: Jewelry Master Personalizer PRO
+ * Description: ניהול חריטה ולוגיקה מותנית עם דשבורד
+ * Version: 1.0
+ */
 
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// טעינת הקבצים מהתיקייה
+require_once plugin_dir_path( __FILE__ ) . 'includes/admin-panel.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/frontend-logic.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/cart-logic.php';
